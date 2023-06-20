62ecb5b4dc0108f5bd1418a01b11317ce6f8def9
Стас Басов <stas.basov@whatever.com>
2023-06-21 01:21:17 +0300

    load

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'djoser',
+    'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
---
+++ backend/users/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+
+from .models import Follows, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    """Администрирование пользователей."""
+
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'date_joined',
+    )
+
+    search_fields = (
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+    )
+
+    list_filter = (
+        'username',
+        'email',
+        'date_joined',
+    )
+
+    ordering = (
+        'username',
+    )
+
+
+class FollowsAdmin(admin.ModelAdmin):
+    """Администрирование подписок."""
+
+    list_display = (
+        'id',
+        'user',
+        'author',
+    )
+
+    search_fields = (
+        'user',
+        'author',
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Follows, FollowsAdmin)
